b4d8bfb86a03c88269cec71ab84d6053b706e4cf
tom hannaford <thom.hannaford@gmail.com>
2019-11-30 20:38:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  create_an_array.pop
+  run add_element_to_end_of_array
+    create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
